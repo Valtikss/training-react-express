803bd34886459b373a862cbe78450525515fcf12
Alexis LAVIGNE <alexis.lavigne@altitudeinfra.fr>
2025-02-17 15:24:52 +0100

    🎉 Init project

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-blue-500">
+        Hello Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,19 @@
+// server/server.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+// Route test
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from Express!' });
+});
+
+// Lancement du serveur
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
