7b0c094c15992276d1e31407108414f78664c643
Aniss <aniss.sej@gmail.com>
2025-02-18 19:19:10 +0100

    le ops est tout nu

---
+++ server/routes/Route.jsx
@@ -0,0 +1,7 @@
+const express = require('express');
+const Controller = require('../controllers/Controller');
+const router = express.Router();
+
+router.get('/', Controller.getTest);
+
+module.exports = router;
