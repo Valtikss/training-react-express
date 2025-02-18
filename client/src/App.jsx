365e56569b8a64eeba27785b157aaed4152f87bd
Couture Léo <leo.couture@live.fr>
2025-02-18 21:29:28 +0100

    J2-1.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold text-blue-500">Bonjour Professeur !</h1>
-      <h1 className="text-xl w-fit px-2 font-bold transition delay-150 duration-300 ease-in-out text-black hover:bg-purple-500 hover:text-white">
-        Je m&apos;appelle Léo et je suis prêt à suivre votre cours
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
