edb112b07a7c3dafa98c5ce25bcbcab3625d4588
Splash Mind <85194871+timo-thi@users.noreply.github.com>
2025-02-23 20:18:22 +0100

    feat(Exo6): page restaurant un peu plus jolie

--- client/src/pages/Restaurant/Restaurant.tsx
+++ client/src/pages/Restaurant/Restaurant.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 import { useParams } from "react-router-dom";
 import { useRestaurant } from "@/hooks";
@@ -18,15 +18,29 @@ const Restaurant = () => {
   if (loading)
     return <Typography variant="h4">Chargement du restaurant...</Typography>;
   if (error) return <Typography variant="h4">{error}</Typography>;
+  if (!restaurant)
+    return <Typography variant="h4">Restaurant non trouvé</Typography>;
 
   return (
-    <Box padding={2}>
-      <Typography variant="h4">{restaurant?.name}</Typography>
-      <Typography variant="body1">{restaurant?.address}</Typography>
-      <Typography variant="body1">Rating: {restaurant?.rating}</Typography>
-      <Typography variant="body1">Phone: {restaurant?.phone}</Typography>
-      <Typography variant="body1">Website: {restaurant?.website}</Typography>
-    </Box>
+    <Stack direction={"row"} spacing={2} alignItems={"center"}>
+      <Box maxWidth={"30%"}>
+        <img src={restaurant.image} alt={restaurant.name} />
+      </Box>
+      <Box padding={2}>
+        <Stack spacing={0} justifyContent="center" paddingBottom={0.5}>
+          <Typography variant="h4" lineHeight={1}>
+            {restaurant.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Restauration {restaurant.cuisine}
+          </Typography>
+        </Stack>
+        <Typography variant="body1">{restaurant.address}</Typography>
+        <Typography variant="body1">Rating: {restaurant.rating}</Typography>
+        <Typography variant="body1">Phone: {restaurant.phone}</Typography>
+        <Typography variant="body1">Website: {restaurant.website}</Typography>
+      </Box>
+    </Stack>
   );
 };
 
